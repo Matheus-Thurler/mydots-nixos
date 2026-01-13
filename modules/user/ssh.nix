83ac1cccf5730b95b55329b6e1f9656f6769b579
Matheus Thurler <contato@matheusthurler.com.br>
2026-01-13 19:57:33 -0300

    feat: adiciona auto-correção de permissões SSH

--- modules/user/ssh.nix
+++ modules/user/ssh.nix
@@ -19,4 +19,33 @@
       };
     };
   };
+
+  # Garante permissões corretas nos arquivos SSH
+  home.activation.fixSshPermissions = ''
+    # Corrige permissões do diretório .ssh
+    if [ -d "$HOME/.ssh" ]; then
+      chmod 700 "$HOME/.ssh"
+      
+      # Corrige permissões do arquivo config
+      if [ -f "$HOME/.ssh/config" ]; then
+        chmod 600 "$HOME/.ssh/config"
+      fi
+      
+      # Corrige permissões das chaves privadas do GitHub
+      if [ -d "$HOME/.ssh/matheus" ]; then
+        chmod 700 "$HOME/.ssh/matheus"
+        if [ -f "$HOME/.ssh/matheus/id_rsa" ]; then
+          chmod 600 "$HOME/.ssh/matheus/id_rsa"
+        fi
+      fi
+      
+      # Corrige permissões das chaves privadas do GitLab
+      if [ -d "$HOME/.ssh/matheus-gitlab" ]; then
+        chmod 700 "$HOME/.ssh/matheus-gitlab"
+        if [ -f "$HOME/.ssh/matheus-gitlab/id_rsa_matheus_gitlab" ]; then
+          chmod 600 "$HOME/.ssh/matheus-gitlab/id_rsa_matheus_gitlab"
+        fi
+      fi
+    fi
+  '';
 }
